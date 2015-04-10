4068ede26df50a6a18f78963e353d3a15faf37ff
Apple-Cider <mbank@kent.edu>
2015-04-10 15:27:30 -0400

    e 1.16

---
+++ exercises/1.16.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(define (square x) (* x x))
+(define (even? x) (= (remainder x 2) 0))
+
+(define (expt b n)
+  (define (expt-iter a b n)
+    (cond ((= n 0) a)
+          ((even? n) (expt-iter a (square b) (/ n 2)))
+          (else (expt-iter (* a b) b (- n 1)))))
+  (expt-iter 1 b n))
