1bfce224e636a1b77663c18ecd27a0f62a802c76
Apple-Cider <mbank@kent.edu>
2015-04-29 14:25:05 -0400

    e 2.21; e 1.26 expanded

---
+++ exercises/2.21.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(define (square x) (* x x))
+
+(define (square-list-a items)
+  (if (null? items)
+      '()
+      (cons (square (car items))
+            (square-list-a (cdr items)))))
+
+(define (square-list-b items)
+  (map square items))
+
+(square-list-a (list 2 3 4 5))
+(square-list-b (list 2 3 4 5))
