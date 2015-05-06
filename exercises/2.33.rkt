a0b15401a97b030fe14fd670c1189caa43ede3a9
Apple-Cider <mbank@kent.edu>
2015-05-06 16:43:56 -0400

    e 2.33 - length impl feels off

---
+++ exercises/2.33.rkt
@@ -0,0 +1,50 @@
+#lang racket
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) null)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      null
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) null)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+
+
+
+(define (map-a p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) null sequence))
+
+(define list-0 (list 1 2 3 4))
+(define list-1 (list 5 6 7 8))
+(define (add-1 x) (+ x 1))
+
+(map add-1 list-0)
+(map-a add-1 list-0)
+
+(define (append-a seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(append list-0 list-1)
+(append-a list-0 list-1)
+
+
+(define (length-a sequence)
+  (accumulate (lambda (x y) (+ 1 (length-a (cdr sequence)))) 0 sequence))
+
+(length list-0)
+(length-a list-0)
