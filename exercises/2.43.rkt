beddeeb578bf52e8f88874dacb027e89a3bc7311
Apple-Cider <mbank@kent.edu>
2015-06-03 13:31:56 -0400

    e 2.43

---
+++ exercises/2.43.rkt
@@ -0,0 +1,117 @@
+#lang racket
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) null)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      null
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (flatmap proc seq)
+  (accumulate append null (map proc seq)))
+
+
+(define (queens board-size)
+  (define (make-position column row) (list column row))
+  (define (column position) (car position))
+  (define (row position) (cadr position))
+  ;(define (print-position position) (newline) (display "col: ") (display (car position)) (display " row: ") (display (cadr position)))
+  ;(define (print-board board) (map (lambda (position) (print-position position)) board))
+  
+  (define empty-board (list))
+  
+  (define (adjoin-position new-row k rest-of-queens)
+    ;(newline) (display "new position: ") (print-position (make-position k new-row))
+    (cons (make-position k new-row) rest-of-queens))
+  
+  (define (safe? k positions)
+    ;(newline) (display "is this board safe? ") (print-board positions) (newline)
+    (safe-iter (car positions) (cdr positions)))
+  (define (safe-iter position positions)
+    (cond ((null? positions) true)
+          ((attacks? position (car positions)) false)
+          (else (safe-iter position (cdr positions)))))
+  (define (attacks? pos1 pos2)
+    ;(newline) (display "attacks? :") (print-position pos1) (print-position pos2)
+    (or (= (row pos1) (row pos2))
+        (= (abs (- (column pos1) (column pos2)))
+           (abs (- (row pos1) (row pos2))))))
+  
+  (define (queen-cols k)
+    ;(newline) (display " running queen-cols; k: ") (display k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (cond ((= 1 new-row) (newline) (display "k: ") (display k) (display " solution count: ") (display (length rest-of-queens))))
+                   (adjoin-position new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+(define (queens-slow board-size)
+  (define (make-position column row) (list column row))
+  (define (column position) (car position))
+  (define (row position) (cadr position))
+  ;(define (print-position position) (newline) (display "col: ") (display (car position)) (display " row: ") (display (cadr position)))
+  ;(define (print-board board) (map (lambda (position) (print-position position)) board))
+  
+  (define empty-board (list))
+  
+  (define (adjoin-position new-row k rest-of-queens)
+    ;(newline) (display "new position: ") (print-position (make-position k new-row))
+    (cons (make-position k new-row) rest-of-queens))
+  
+  (define (safe? k positions)
+    ;(newline) (display "is this board safe? ") (print-board positions) (newline)
+    (safe-iter (car positions) (cdr positions)))
+  (define (safe-iter position positions)
+    (cond ((null? positions) true)
+          ((attacks? position (car positions)) false)
+          (else (safe-iter position (cdr positions)))))
+  (define (attacks? pos1 pos2)
+    ;(newline) (display "attacks? :") (print-position pos1) (print-position pos2)
+    (or (= (row pos1) (row pos2))
+        (= (abs (- (column pos1) (column pos2)))
+           (abs (- (row pos1) (row pos2))))))
+  
+  (define (queen-cols k)
+    ;(newline) (display " running queen-cols; k: ") (display k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (new-row)
+            (map (lambda (rest-of-queens)
+                   ;(cond ((= 1 new-row) (newline) (display "k: ") (display k) (display " solution count: ") (display (length rest-of-queens))))
+                   (adjoin-position new-row k rest-of-queens))
+                 (queen-cols (- k 1))))
+          (enumerate-interval 1 board-size)))))
+  (queen-cols board-size))
+
+(define (timed-queen-test queen-function board-size)
+  (let ((start-time (current-milliseconds))
+        (solution (queen-function board-size))
+        (end-time (current-milliseconds)))
+    (display "elapsed time (ms): ")
+    (- end-time start-time)))
+
+;(timed-queen-test queens 8)
+;(timed-queen-test queens-slow 8)
+
+;(queens 8)
